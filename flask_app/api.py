3202837cc86e0086db8a53fd10d5dfa9db5be3f1
FruitVodka <mailtovarsha.v@gmail.com>
2019-11-24 23:22:03 +0530

    answers sorted based on upvote count

--- flask_app/api.py
+++ flask_app/api.py
@@ -14,20 +14,27 @@ cors = CORS(app, resources={r'/api/*': {'origins': '*'}})
 mongo_client = pymongo.MongoClient('localhost', 27017)
 mongo_db = mongo_client['SE']
 
-
 def convertCursor(info):
     data = []
     for x in info:
         data.append(x)
     return data
 
+def sortUpvotes(answer):
+    if 'upvotes' in answer:
+        return answer['upvote_count']
+    else:
+        return 0
+
 @app.route('/api/v1/question',methods=['GET','POST','DELETE'])
 def question():
     if request.method == 'POST':
-        table = mongo_db['question']
-        data = request.get_json()
+        # getting timestamp to set the question's (last-modified) timestamp
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        # getting data
+        data = request.get_json()
+        table = mongo_db['question']
         data['timestamp'] = dt_string
         data['answer'] = []
         val = table.insert(data)
@@ -74,15 +81,16 @@ def validated():
 @app.route('/api/v1/answer',methods=['GET','POST','DELETE'])
 def answer():
     if request.method == 'POST':
-        table = mongo_db['question']
+        # getting timestamp to update the question's (last-modified) timestamp
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        # getting data
         data = request.get_json()
+        table = mongo_db['question']
         # making the answer JSON to append to answers array of particular question
         details_to_add = {}
         details_to_add['email'] = data['email']
         details_to_add['answer'] = data['answer']
-        # getting timestamp to update the question's (last-modified) timestamp
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         # searching for question
         question = {'question': data['question']}
         val = table.find(question)
@@ -106,6 +114,10 @@ def answer():
             return jsonify({}),400
     
     elif request.method == 'DELETE':
+        # getting timestamp to update the question's (last-modified) timestamp
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        # getting data
         data = request.get_json()
         table = mongo_db['question']
         question = {'question': data['question']}
@@ -131,7 +143,7 @@ def answer():
             return jsonify({}),400
 
         # updating the question by deleting answer
-        newvalues = {'$set': {'answer': updated_answers}}
+        newvalues = {'$set': {'answer': updated_answers, 'timestamp': dt_string}}
         val = table.update_one(question, newvalues)
         if val:
             resp = {}
@@ -145,6 +157,10 @@ def answer():
 @app.route('/api/v1/answer/upvote',methods=['POST'])
 def upvote():
     if request.method == 'POST':
+        # getting timestamp to update the question's (last-modified) timestamp
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        # getting request data
         data = request.get_json()
         table = mongo_db['question']
         question = {'question': data['question']}
@@ -167,15 +183,20 @@ def upvote():
                     upvoted_users = set(existing_answers[i]['upvotes'])
                     upvoted_users.add(user)
                     existing_answers[i]['upvotes'] = list(upvoted_users)
+                    existing_answers[i]['upvote_count'] += 1
                 else:
                     existing_answers[i]['upvotes'] = [user]
+                    existing_answers[i]['upvote_count'] = 1
                 break
         if not found:
             # answer not found
             return jsonify({}),400
         
+        # sorting answer list based on number of upvotes
+        existing_answers.sort(key = sortUpvotes, reverse = True)
+
         # updating question
-        newvalues = {'$set': {'answer': existing_answers}}
+        newvalues = {'$set': {'answer': existing_answers, 'timestamp': dt_string}}
         val = table.update_one(question, newvalues)
         if val:
             resp = {}
@@ -193,14 +214,6 @@ def feed():
         val = table.find({},{'_id':False})
         val = convertCursor(val)
         val.sort(key = lambda x: x['timestamp'], reverse = True)
-        # response = []
-        # for v in val:
-        #     to_add = {}
-        #     to_add['question'] = v['question']
-        #     to_add['ques_email'] = v['ques_email']
-        #     to_add['timestamp'] = v['timestamp']
-        #     to_add['answer'] = v['answer']
-        #     response.append(to_add)
         return jsonify(val),200
     else:
         return jsonify({}),405
